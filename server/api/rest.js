2033bd72dc297c4208c33cf3cddecd335467abca
NROL39 <mrjukin@gmail.com>
2015-11-09 00:27:55 +0200

    nitro tsts

--- server/api/rest.js
+++ server/api/rest.js
@@ -153,8 +153,11 @@ function processNitroBit(url,passwords,doc){
   	});
     //http://www.nitrobit.net/ajax/unlock.php?password=$password&file=$fileid&keep=true
   	horseman
-  	.userAgent(getRandString(64))
-  	.open('http://www.nitrobit.net/ajax/unlock.php?password='+password+'&file='+fileid)
+    .userAgent('Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16')
+		.headers({
+      'X-Requested-With' : 'XMLHttpRequest'
+    })
+  	.open('http://www.nitrobit.net/ajax/unlock.php?password='+password+'&file='+fileid+'&keep=false')
     .html()
     .then(function(html){
       pf = html.indexOf('href') > -1;
--- server/ext/providers/sdarot.js
+++ server/ext/providers/sdarot.js
@@ -39,10 +39,10 @@ Sdarot.on('processURL',function(doc) {
   	});
     _horseman
     .userAgent('Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16')
-    .post('http://mobile.sdarot.wf/mobile.php?do=searchServer','vid='+firstRun.error.VID)
-    .headers({
+		.headers({
       'X-Requested-With' : 'XMLHttpRequest'
     })
+    .post('http://mobile.sdarot.wf/mobile.php?do=searchServer','vid='+firstRun.error.VID)
     .html("body")
     .then(function(html){
 			doc.providerId = Sdarot.id;
