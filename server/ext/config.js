36412df0639a0eeb79e9bbd3ab1c421c496c082a
NROL39 <mrjukin@gmail.com>
2015-11-11 01:33:42 +0200

    Registration disabled

--- server/ext/config.js
+++ server/ext/config.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 Config = {
   //BASE_PATH: '/Users/cipher/Developer/Meteor/'
-  BASE_PATH: fs.existsSync('/ext') ? '/ext/' : '/Users/cipher/Developer/Meteor/',
+  BASE_PATH: fs.existsSync('/ext') ? '/ext/' : '/home/cipher/Development/Meteor/',
   NITRO_PASSWORDS: ['NB4HRBZ5JVFD'],
   proxies: []
 };
--- server/publish/downloads.js
+++ server/publish/downloads.js
@@ -1,5 +1,6 @@
 Meteor.publish("all_downloads", function() {
 	if(this.userId){
+		//publish to logged in users only!
 		return Downloads.find({}, {sort:{createdAt:-1}});
 	}
 });
