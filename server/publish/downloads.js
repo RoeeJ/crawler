8d86b69bb8ca0d9a18af8f56219f4b61b09b831e
NROL39 <mrjukin@gmail.com>
2015-11-11 01:26:30 +0200

    publish mod

--- server/publish/downloads.js
+++ server/publish/downloads.js
@@ -1,16 +1,5 @@
 Meteor.publish("all_downloads", function() {
-	return Downloads.find({}, {sort:{createdAt:-1}});
+	if(this.userId){
+		return Downloads.find({}, {sort:{createdAt:-1}});
+	}
 });
-
-Meteor.publish("in_progress", function() {
-	return Downloads.find({status:1}, {sort:{createdAt:-1}});
-});
-
-Meteor.publish("completed_downloads", function() {
-	return Downloads.find({status:2}, {sort:{createdAt:-1}});
-});
-
-Meteor.publish("unclaimed_download", function() {
-	return Downloads.find({status:0}, {sort:{createdAt:1}});
-});
-
